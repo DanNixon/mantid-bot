99a98a0d3361473772c013b4083b0924c287c12c
Harry Jeffery <harry@exec64.co.uk>
2014-07-17 21:29:51 +0100

    Move global variables into single block and ran go fmt

--- mantid-bot.go
+++ mantid-bot.go
@@ -1,122 +1,124 @@
 package main
 
 import (
-  "github.com/thoj/go-ircevent"
-  "fmt"
-  "regexp"
-  "net/http"
-  "io/ioutil"
-  "code.google.com/p/go.net/html"
-  "strings"
+	"code.google.com/p/go.net/html"
+	"fmt"
+	"github.com/thoj/go-ircevent"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
 )
 
-var roomName = "#mantid-talk"
-var tracURL = "http://trac.mantidproject.org/mantid/ticket/"
+var (
+	roomName = "#mantid-talk"
+	tracURL  = "http://trac.mantidproject.org/mantid/ticket/"
 
-var ticketNumberMatcher = regexp.MustCompile(`#\d{4}`)
-var ticketTitleMatcher = regexp.MustCompile(`\((.*?)\)`)
+	ticketNumberMatcher = regexp.MustCompile(`#\d{4}`)
+	ticketTitleMatcher  = regexp.MustCompile(`\((.*?)\)`)
 
-var con = irc.IRC("mantid-bot", "mantid-bot")
+	con = irc.IRC("mantid-bot", "mantid-bot")
+)
 
 func main() {
-  //Connect to Freenode
-  err := con.Connect("irc.freenode.net:6667");
-  if err != nil {
-    fmt.Println("Failed connecting");
-    return;
-  }
+	//Connect to Freenode
+	err := con.Connect("irc.freenode.net:6667")
+	if err != nil {
+		fmt.Println("Failed connecting")
+		return
+	}
 
-  //Join channel
-  con.AddCallback("001", func (e *irc.Event) {
-    con.Join(roomName);
-  })
+	//Join channel
+	con.AddCallback("001", func(e *irc.Event) {
+		con.Join(roomName)
+	})
 
-  //Handle new messages
-  con.AddCallback("PRIVMSG", handleMessage);
+	//Handle new messages
+	con.AddCallback("PRIVMSG", handleMessage)
 
-  con.Loop()
+	con.Loop()
 }
 
 func handleMessage(e *irc.Event) {
-  //Try to extract a Trac ticket number from message
-  ticketString := ticketNumberMatcher.FindString(e.Message());
+	//Try to extract a Trac ticket number from message
+	ticketString := ticketNumberMatcher.FindString(e.Message())
 
-  //Got a message with a Trac ticket
-  if(ticketString != "") {
-    ticketURL := tracURL + ticketString[1:]
-    ticketTitle, ticketStatus := getTicketInfo(ticketURL);
+	//Got a message with a Trac ticket
+	if ticketString != "" {
+		ticketURL := tracURL + ticketString[1:]
+		ticketTitle, ticketStatus := getTicketInfo(ticketURL)
 
-    if ticketTitle != "" {
-      con.Privmsg(roomName, fmt.Sprintf("%s: %s (%s)", ticketString, ticketTitle, ticketStatus))
-      con.Privmsg(roomName, fmt.Sprintf("%s: %s", ticketString, ticketURL))
-    } else {
-      con.Privmsg(roomName, fmt.Sprintf(
-        "There are over 9000 tickets, but %s is not one of them", ticketString))
-    }
-  }
+		if ticketTitle != "" {
+			con.Privmsg(roomName, fmt.Sprintf("%s: %s (%s)", ticketString, ticketTitle, ticketStatus))
+			con.Privmsg(roomName, fmt.Sprintf("%s: %s", ticketString, ticketURL))
+		} else {
+			con.Privmsg(roomName, fmt.Sprintf(
+				"There are over 9000 tickets, but %s is not one of them", ticketString))
+		}
+	}
 }
 
 func getTicketInfo(url string) (string, string) {
-  r, err := http.Get(url)
-  if(err != nil) {
-    return "", ""
-  }
+	r, err := http.Get(url)
+	if err != nil {
+		return "", ""
+	}
 
-  body, err := ioutil.ReadAll(r.Body)
-  if(err != nil) {
-    return "", ""
-  }
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return "", ""
+	}
 
-  doc, err := html.Parse(strings.NewReader(string(body)))
-  if err != nil {
-    return "", ""
-  }
+	doc, err := html.Parse(strings.NewReader(string(body)))
+	if err != nil {
+		return "", ""
+	}
 
-  return htmlFindTitle(doc), htmlFindStatus(doc);
+	return htmlFindTitle(doc), htmlFindStatus(doc)
 }
 
 func htmlFindTitle(n *html.Node) string {
-  if n.Type == html.ElementNode && n.Data == "title" {
-    rawTitle := ticketTitleMatcher.FindString(n.FirstChild.Data)
-    if rawTitle == "" {
-      return ""
-    }
-    title := rawTitle[1:len(rawTitle)-1]
+	if n.Type == html.ElementNode && n.Data == "title" {
+		rawTitle := ticketTitleMatcher.FindString(n.FirstChild.Data)
+		if rawTitle == "" {
+			return ""
+		}
+		title := rawTitle[1 : len(rawTitle)-1]
 
-    return title
-  }
+		return title
+	}
 
-  for c := n.FirstChild; c != nil; c = c.NextSibling {
-    res := htmlFindTitle(c)
-    if res != "" {
-      return res;
-    }
-  }
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		res := htmlFindTitle(c)
+		if res != "" {
+			return res
+		}
+	}
 
-  return ""
+	return ""
 }
 
 func htmlFindStatus(n *html.Node) string {
-  if n.Type == html.ElementNode && n.Data == "span" {
-    for _, a := range n.Attr {
-      if a.Key == "class" && a.Val == "status" {
-        rawStatus := ticketTitleMatcher.FindString(n.FirstChild.Data)
-        if rawStatus == "" {
-          return ""
-        }
-        status := rawStatus[1:len(rawStatus)-1]
+	if n.Type == html.ElementNode && n.Data == "span" {
+		for _, a := range n.Attr {
+			if a.Key == "class" && a.Val == "status" {
+				rawStatus := ticketTitleMatcher.FindString(n.FirstChild.Data)
+				if rawStatus == "" {
+					return ""
+				}
+				status := rawStatus[1 : len(rawStatus)-1]
 
-        return status
-      }
-    }
-  }
+				return status
+			}
+		}
+	}
 
-  for c := n.FirstChild; c != nil; c = c.NextSibling {
-    res := htmlFindStatus(c)
-    if res != "" {
-      return res;
-    }
-  }
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		res := htmlFindStatus(c)
+		if res != "" {
+			return res
+		}
+	}
 
-  return ""
+	return ""
 }
